711d5995c69dae0c21c28d65b2feddf4d3332053
humanscrew <1084465086@qq.com>
2021-10-02 12:19:32 +0800

    #20211002

--- myapi/models/__init__.py
+++ myapi/models/__init__.py
@@ -13,6 +13,7 @@ from myapi.models.company import (
 )
 from myapi.models.role import Role, PermitCode
 from myapi.models.route import Route, RouteMeta, RouteTree, RouteClosureTable
+from myapi.models.ticket import TicketLaiu8, TicketLaiu8Refund
 
 __all__ = [
     "TokenBlocklist",
@@ -26,4 +27,5 @@ __all__ = [
     "Map_Cooperate_Company",
     "Role", "PermitCode",
     "Route", "RouteMeta", "RouteTree", "RouteClosureTable",
+    "TicketLaiu8", "TicketLaiu8Refund",
 ]
