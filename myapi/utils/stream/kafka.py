b75873689a3c595f0539b2ec2b39d50286fe4152
humanscrew <1084465086@qq.com>
2021-11-18 08:17:49 +0800

    upgrade python3.10

--- myapi/utils/stream/kafka.py
+++ myapi/utils/stream/kafka.py
@@ -47,7 +47,7 @@ class Kafka:
         )
         try:
             future.get(timeout=10)
-        except kafka_errors:
+        except Exception as e:
             traceback.format_exc()
 
     def consumer(self, topic=None, group_id=None):
