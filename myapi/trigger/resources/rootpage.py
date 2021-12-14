05fba5a779a315357077d7b3a8b9df06db96fe5b
humanscrew <1084465086@qq.com>
2021-12-14 21:12:34 +0800

    edit rootpage

--- myapi/trigger/resources/rootpage.py
+++ myapi/trigger/resources/rootpage.py
@@ -4,4 +4,4 @@ from flask_restful import Resource
 class RootPage(Resource):
     @staticmethod
     def get():
-        return "<html><body>westhideAPI success!</body></html>"
+        return "westhideAPI success!"
